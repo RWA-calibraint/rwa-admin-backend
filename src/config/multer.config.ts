c1903a4ec2bbdcda3e70772bfa78696f1d893cd6
Kaisar bhat <kaisarahmad@calibraint.com>
2026-02-16 19:42:40 +0530

    chore: file storage

--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -4,7 +4,7 @@ import { diskStorage } from 'multer';
 
 export const multerConfig = {
   storage: diskStorage({
-    destination: './uploads',
+    destination: '/tmp',
     filename: (req, file, callback) => {
       const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
       callback(
